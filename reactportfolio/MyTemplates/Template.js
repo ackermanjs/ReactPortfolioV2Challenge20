19928c183097181b389eae2c50e8393fd2e7f570
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-05 12:12:38 -0700

    creating a template that i can use to keep my component structure and css styling more consistent.

--- reactportfolio/MyTemplates/Template.js
+++ reactportfolio/MyTemplates/Template.js
@@ -1,1 +1,30 @@
 //Creating a template that I can use for my components to stay consistent with the format.
+import React from 'react'
+import "../components/.css";
+
+const Template = () => {
+  return (
+  <div className='page'>
+    <div className='header'>
+        <h2 className='header-title'></h2>
+    </div>
+    <div className='content'>
+        <p className='content-text'></p>
+    </div>
+    <div className='button-contaier'>
+        {/* this is where a button would go, if the page features a button. */}
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+  )
+}
+
+export default Template
