c7e6f0a8c3dbfa4defac48d8cd52c0c3f7ba43fe
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-05 12:29:40 -0700

    updated my template structure. Created aboutpage js + css

--- reactportfolio/MyTemplates/Template.js
+++ reactportfolio/MyTemplates/Template.js
@@ -1,30 +1,28 @@
 //Creating a template that I can use for my components to stay consistent with the format.
-import React from 'react'
+import React from "react";
 import "../components/.css";
 
 const Template = () => {
   return (
-  <div className='page'>
-    <div className='header'>
-        <h2 className='header-title'></h2>
+    <div className="page">
+      <div className="page-container">
+          <div className="header-container">
+            <div className="header">
+              <h2 className="header-title"></h2>
+            </div>
+          </div>
+          <div className="content-container">
+            <div className="content">
+              <p className="content-text">
+              </p>
+            </div>
+            <div className="button-contaier">
+              {/* this is where a button would go, if the page features a button. */}
+            </div>
+          </div>
+      </div>
     </div>
-    <div className='content'>
-        <p className='content-text'></p>
-    </div>
-    <div className='button-contaier'>
-        {/* this is where a button would go, if the page features a button. */}
-    </div>
-  </div>
+  );
+};
 
-
-
-
-
-
-
-
-
-  )
-}
-
-export default Template
+export default Template;
